549ad8be58f5c2aa2acd8c3a533317116dc0be2a
Huai-Che Lu <hc.acsalu@gmail.com>
2016-11-04 12:29:04 -0400

    Add jest

--- __tests__/MenuScreen.js
+++ __tests__/MenuScreen.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import MenuScreen from '../components/MenuScreen';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+      <MenuScreen />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- __tests__/NewFollowScreen.js
+++ __tests__/NewFollowScreen.js
@@ -1,13 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Realm from 'realm';
-import Index from '../index.android.js';
+import NewFollowScreen from '../components/NewFollowScreen';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+      <NewFollowScreen />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
