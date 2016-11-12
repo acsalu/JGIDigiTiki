52ffb642a548a58559a4f2d5a0e9f4d8119ad7f6
Huai-Che Lu <hc.acsalu@gmail.com>
2016-11-12 12:27:49 -0500

    Add fastlane and crashlytics

--- android/app/src/main/java/com/jgidigitiki/MainApplication.java
+++ android/app/src/main/java/com/jgidigitiki/MainApplication.java
@@ -9,11 +9,19 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Fabric.with(this, new Crashlytics());
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
